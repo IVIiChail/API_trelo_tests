cc715056f0365fb37563b8df4c5ce8c47ba98a96
Olena <elenamelnik1856@gmail.com>
2023-03-02 11:48:09 +0200

    Add API test Get-a-Card

--- src/test/java/trelloAPI/POST/CreateNewCardTest.java
+++ src/test/java/trelloAPI/POST/CreateNewCardTest.java
@@ -1,28 +1,37 @@
 package trelloAPI.POST;
 
+import org.testng.annotations.AfterTest;
 import org.testng.annotations.Test;
 import io.restassured.path.json.JsonPath;
 import org.testng.Assert;
+import trelloAPI.DELETE.DeleteCardTest;
 import trelloAPI.Specifications;
 import trelloAPI.Globals;
 
 import static io.restassured.RestAssured.given;
 
-public class CreateNewCard {
+public class CreateNewCardTest {
     public static String ID_CARD;
     @Test
-    public void createNewCard(){
+    public void createNewCardTest(){
         Specifications.installSpec(Specifications.requestSpec(), Specifications.responseSpecOK200());
         JsonPath jsonResponse = given()
                 .header("Accept", "application/json")
                 .queryParam("idList", Globals.ID_LIST)
                 .when()
                 .post("/1/cards")
-                .jsonPath();
+                .then().log().all()
+                .extract().jsonPath();
 
-        ID_CARD = jsonResponse.get("idList");
-        System.out.println(jsonResponse.get("idList").toString());
+        ID_CARD = jsonResponse.get("id");
         Assert.assertEquals(jsonResponse.get("idList").toString(),Globals.ID_LIST);
 
+    }
+
+    @AfterTest
+    public void deleteCard(){
+        DeleteCardTest deleteCardTest = new DeleteCardTest();
+        deleteCardTest.CARD_ID = ID_CARD;
+        deleteCardTest.deleteCardTest();
     }
 }
