4804cbdba52c47c565cd5104f3b539b74a86ff52
Olena <elenamelnik1856@gmail.com>
2023-03-03 16:12:45 +0200

    Create Api test Get CheckItems on a Card

---
+++ src/test/java/trelloAPI/GET/GetCheckItemsOnACardTest.java
@@ -0,0 +1,43 @@
+package trelloAPI.GET;
+
+import io.restassured.path.json.JsonPath;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+import trelloAPI.DELETE.DeleteCardTest;
+import trelloAPI.POST.CreateNewCardTest;
+import trelloAPI.Specifications;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.greaterThanOrEqualTo;
+
+public class GetCheckItemsOnACardTest {
+    public String CARD_ID;
+
+    @BeforeTest
+    public void createNewCard(){
+        CreateNewCardTest createNewCardTest = new CreateNewCardTest();
+        createNewCardTest.createNewCardTest();
+        CARD_ID = createNewCardTest.ID_CARD;
+    }
+
+    @Test
+    public void getCheckItemsOnACard(){
+        Specifications.installSpec(Specifications.requestSpec(),Specifications.responseSpecOK200());
+        JsonPath jsonResponse = given()
+                .header("Accept", "application/json")
+                .when()
+                .get("/1/cards/{id}/checkItemStates", CARD_ID)
+                .then()
+                .body("size", greaterThanOrEqualTo(0))
+                .log().all()
+                .extract().jsonPath();
+    }
+
+    @AfterTest
+    public void deleteCardField(){
+        DeleteCardTest deleteCardTest = new DeleteCardTest();
+        deleteCardTest.CARD_ID = CARD_ID;
+        deleteCardTest.deleteCardTest();
+    }
+}
